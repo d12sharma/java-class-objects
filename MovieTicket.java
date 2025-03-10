6fd0b3b6b03d8f314a44a498e8d626528b69fb30
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-10 20:03:14 +0530

    Dhruv Sharma Add:movie ticket

---
+++ MovieTicket.java
@@ -0,0 +1,30 @@
+class MovieTicket {
+    private String movieName;
+    private int seatNumber;
+    private double price;
+
+    public MovieTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+    }
+
+    public void bookTicket(String movieName, int seatNumber, double price) {
+        this.movieName = movieName;
+        this.seatNumber = seatNumber;
+        this.price = price;
+        System.out.println("Ticket booked successfully!");
+    }
+
+    public void displayTicketDetails() {
+        System.out.println("Movie Ticket Details:");
+        System.out.println("Movie Name: " + movieName);
+        System.out.println("Seat Number: " + seatNumber);
+        System.out.println("Price: " + price);
+    }
+
+    public static void main(String[] args) {
+        MovieTicket ticket = new MovieTicket("Animal", 45, 450);
+        ticket.displayTicketDetails();
+    }
+}
