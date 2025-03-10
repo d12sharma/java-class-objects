7b9705f7183746bd3d50543d017becef86416074
dhruvsharma123 <dhruv1931.be21@chitkara.edu.in>
2025-03-10 18:39:52 +0530

    Dhruv Sharma Add:Area Of Circle

---
+++ AreaOfCircle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class  AreaOfCircle{
+	private int radius;
+	
+	public AreaOfCircle (int radius){
+		this.radius = radius;
+	}
+	
+	public void displayDetails(){
+		System.out.println("The Area of circle is :"+Math.PI*radius*radius);
+		System.out.println("The Circumference of circle is "+ 2*Math.PI*radius);
+	}
+	public static void main(String[] args){
+		AreaOfCircle area1 = new AreaOfCircle(10);
+		area1.displayDetails();
+	}
+	
+}
+/*
+Output
+The Area of circle is :314.1592653589793
+The Circumference of circle is 62.83185307179586
+*/
